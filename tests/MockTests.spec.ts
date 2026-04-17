d3b9c8c888226bc0bc09f770cdcb1f7d4d0a8954
Javier Gerard <javiergerard1986@gmail.com>
2026-04-17 11:50:05 -0300

    Adding fake info to a real API response and add videos to reports

--- tests/MockTests.spec.ts
+++ tests/MockTests.spec.ts
@@ -14,6 +14,25 @@ test('Mock a real API providing a value that is not contained in the response', 
     await expect (page.getByText('Dragon Fruit')).toBeVisible();
 });
 
+test('Get real response and add a value to the response', async ({ page }) => {
+    //Get reponse and add something extra
+    await page.route('*/**/api/v1/fruits', async route => {
+        const response = await route.fetch();
+        const json = await response.json();
+        json.push({ name: 'Dragon Fruit', id: 29 });
+        // Adding something extra to the response
+        await route.fulfill({ response, json });
+    });
+
+    //Navigate to the page
+    await page.goto('https://demo.playwright.dev/api-mocking');
+
+    //Validate that the added response is present in the real response
+    await expect(page.getByText('Dragon Fruit', { exact: true })).toBeVisible();
+
+});
+
+
 test.afterEach(async ({ page }) => {
     await page.close();
 })
